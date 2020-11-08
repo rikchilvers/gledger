27b1fc629a363c25bfb29c24dee143a5c8801676
Rik Chilvers <rikchilvers@fastmail.com>
2020-11-08 10:57:52 +0000

    add a todo

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-import (
-	"github.com/rikchilvers/gledger/cmd"
-)
+import "github.com/rikchilvers/gledger/cmd"
+
+// TODO: make slices not take pointers (https://philpearl.github.io/post/bad_go_slice_of_pointers/)
 
 func main() {
 	cmd.Execute()
