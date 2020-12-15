3dc7ec60594d80be09c9d80a876db3ee32627989
Rik Chilvers <rikchilvers@fastmail.com>
2020-12-15 11:18:58 +0000

    add begin/end date flags

--- cmd/root.go
+++ cmd/root.go
@@ -6,19 +6,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootJournalPath string
+var (
+	rootJournalPath string
+	beginDate       string
+	endDate         string
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "gledger",
 	Short: "gledger - command line budgeting",
 	Long:  "gledger is a reimplementation of Ledger in Go\nwith YNAB-style budgeting at its core",
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, _ []string) {
 		cmd.Help()
 	},
 }
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&rootJournalPath, "file", "f", "", "journal file to read (default $LEDGER_FILE)")
+	rootCmd.PersistentFlags().StringVarP(&beginDate, "begin", "b", "", "include only transactions on or after this date")
+	rootCmd.PersistentFlags().StringVarP(&endDate, "end", "e", "", "include only transactions before this date")
 }
 
 // Execute runs gledger
