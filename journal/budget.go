8760db70663bf42fd0d86458b293ab480732e91a
Rik Chilvers <rikchilvers@fastmail.com>
2020-12-27 14:32:55 +0000

    add expense postings to the envelope

--- journal/budget.go
+++ journal/budget.go
@@ -16,51 +16,33 @@ func newBudget() Budget {
 }
 
 type BudgetMonth struct {
-	EnvelopeRoot   *Account
-	incomePostings []*Posting // posting from Income:**:*
-	income         Amount     // sum of income postings amounts
-	overspending   Amount     // flows from previous month
-	future         Amount     // sum of accounts in future months' buckets
+	EnvelopeRoot *Account
+	Income       *Account
+	overspending Amount // flows from previous month
+	future       Amount // sum of accounts in future months' buckets
 }
 
 func newBudgetMonth() BudgetMonth {
+	income := NewAccount(IncomeID)
+	income.Name = "Funds"
 	return BudgetMonth{
-		EnvelopeRoot:   NewAccount(BudgetRootID),
-		incomePostings: make([]*Posting, 0, 10),
+		EnvelopeRoot: NewAccount(BudgetRootID),
+		Income:       income,
 	}
 }
 
 func (b *Budget) addEnvelopePosting(p *Posting) error {
-	month, found := b.Months[normaliseToMonth(p.Transaction.Date)]
-	if !found {
-		month = newBudgetMonth()
-	}
-
-	if p.AccountPath == BudgetRootID {
-		month.EnvelopeRoot.Amount.Add(p.Amount)
-	} else {
-		if err := wireUpPosting(month.EnvelopeRoot, p.Transaction, p); err != nil {
-			return err
-		}
-	}
-
-	b.Months[normaliseToMonth(p.Transaction.Date)] = month
-	return nil
-}
-
-func (b *Budget) addIncomePosting(p *Posting) error {
 	bm, found := b.Months[normaliseToMonth(p.Transaction.Date)]
 	if !found {
 		bm = newBudgetMonth()
 	}
 
-	bm.incomePostings = append(bm.incomePostings, p)
-	// We subtract to make the income positive
-	if bm.income.Commodity == "" {
-		bm.income.Commodity = p.Amount.Commodity
-	}
-	if err := bm.income.Subtract(p.Amount); err != nil {
-		return err
+	if p.AccountPath == BudgetRootID {
+		bm.EnvelopeRoot.Amount.Add(p.Amount)
+	} else {
+		if err := wireUpPosting(bm.EnvelopeRoot, p.Transaction, p); err != nil {
+			return err
+		}
 	}
 
 	b.Months[normaliseToMonth(p.Transaction.Date)] = bm
@@ -73,13 +55,14 @@ func (b *Budget) addExpensePosting(p *Posting) error {
 		bm = newBudgetMonth()
 	}
 
-	// strip 'Expenses' from the path components
-	pathComponents := p.Account.PathComponents[1:]
-	account := bm.EnvelopeRoot.FindOrCreateAccount(pathComponents)
-
 	if bm.EnvelopeRoot.Amount.Commodity == "" {
 		bm.EnvelopeRoot.Amount.Commodity = p.Amount.Commodity
 	}
+
+	// strip 'Expenses' from the path components
+	pathComponents := p.Account.PathComponents[1:]
+	account := bm.EnvelopeRoot.FindOrCreateAccount(pathComponents)
+	account.Postings = append(account.Postings, p)
 
 	// We can use whether the commodity is set to determine if we need to 'initialise' the account
 	if account.Amount.Commodity == "" {
@@ -98,6 +81,34 @@ func (b *Budget) addExpensePosting(p *Posting) error {
 		}
 		return nil
 	}); err != nil {
+		return err
+	}
+
+	b.Months[normaliseToMonth(p.Transaction.Date)] = bm
+	return nil
+}
+
+func (b *Budget) addIncomePosting(p *Posting) error {
+	bm, found := b.Months[normaliseToMonth(p.Transaction.Date)]
+	if !found {
+		bm = newBudgetMonth()
+	}
+
+	if bm.Income.Amount.Commodity == "" {
+		bm.Income.Amount.Commodity = p.Amount.Commodity
+	}
+	if bm.EnvelopeRoot.Amount.Commodity == "" {
+		bm.EnvelopeRoot.Amount.Commodity = p.Amount.Commodity
+	}
+
+	bm.Income.Postings = append(bm.Income.Postings, p)
+
+	// We subtract to make the income positive
+	if err := bm.Income.Amount.Subtract(p.Amount); err != nil {
+		return err
+	}
+
+	if err := bm.EnvelopeRoot.Amount.Subtract(p.Amount); err != nil {
 		return err
 	}
 
