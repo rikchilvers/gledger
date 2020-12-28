779b590d8f6c2f7355aadb67c626313220f160de
Rik Chilvers <rikchilvers@fastmail.com>
2020-12-28 21:32:36 +0000

    track expenses separately to envelope allocations

--- journal/budget.go
+++ journal/budget.go
@@ -1,12 +1,13 @@
 package journal
 
 import (
+	"strings"
 	"time"
 )
 
 // Budget is a wrapper around accounts to enable monthly tracking
 type Budget struct {
-	Months map[time.Time]BudgetMonth // metadata about each month
+	Months map[time.Time]BudgetMonth // what was budgeted
 }
 
 func newBudget() Budget {
@@ -17,6 +18,7 @@ func newBudget() Budget {
 
 type BudgetMonth struct {
 	EnvelopeRoot *Account
+	ExpenseRoot  *Account
 	Income       *Account
 	overspending Amount // flows from previous month
 	future       Amount // sum of accounts in future months' buckets
@@ -25,8 +27,11 @@ type BudgetMonth struct {
 func newBudgetMonth() BudgetMonth {
 	income := NewAccount(IncomeID)
 	income.Name = "Funds"
+	expenses := NewAccount(ExpensesID)
+	expenses.Name = "Spending"
 	return BudgetMonth{
 		EnvelopeRoot: NewAccount(BudgetRootID),
+		ExpenseRoot:  expenses,
 		Income:       income,
 	}
 }
@@ -37,15 +42,56 @@ func (b *Budget) addEnvelopePosting(p *Posting) error {
 		bm = newBudgetMonth()
 	}
 
+	defer func() {
+		b.Months[normaliseToMonth(p.Transaction.Date)] = bm
+	}()
+
 	if p.AccountPath == BudgetRootID {
-		bm.EnvelopeRoot.Amount.Add(p.Amount)
-	} else {
-		if err := wireUpPosting(bm.EnvelopeRoot, p.Transaction, p); err != nil {
-			return err
-		}
+		bm.EnvelopeRoot.Amount.Add(*p.Amount)
+		return nil
 	}
 
-	b.Months[normaliseToMonth(p.Transaction.Date)] = bm
+	if bm.EnvelopeRoot.Amount.Commodity == "" {
+		bm.EnvelopeRoot.Amount.Commodity = p.Amount.Commodity
+	}
+	if bm.ExpenseRoot.Amount.Commodity == "" {
+		bm.ExpenseRoot.Amount.Commodity = p.Amount.Commodity
+	}
+
+	pathComponents := strings.Split(p.AccountPath, ":")
+
+	// Make the envelope account
+	envelopeAccount := bm.EnvelopeRoot.FindOrCreateAccount(pathComponents)
+	envelopeAccount.Amount.Commodity = p.Amount.Commodity
+
+	// Make the expense account
+	expenseAccount := bm.ExpenseRoot.FindOrCreateAccount(pathComponents)
+	expenseAccount.Amount.Commodity = p.Amount.Commodity
+
+	// Add to the envelope account
+	envelopeAccount.Postings = append(envelopeAccount.Postings, p)
+
+	// As this account is not the same as the non-budget expenses account version
+	// we need to ask it to create its path as it drops the 'Expenses:' head
+	if len(envelopeAccount.Path) == 0 {
+		envelopeAccount.Path = envelopeAccount.CreatePath()
+		envelopeAccount.PathComponents = pathComponents
+	}
+	if len(expenseAccount.Path) == 0 {
+		expenseAccount.Path = expenseAccount.CreatePath()
+		expenseAccount.PathComponents = pathComponents
+	}
+
+	// Add the posting's amount to the envelope account and all of its ancestors
+	if err := envelopeAccount.WalkAncestors(func(a *Account) error {
+		if err := a.Amount.Add(*p.Amount); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -58,25 +104,42 @@ func (b *Budget) addExpensePosting(p *Posting) error {
 	if bm.EnvelopeRoot.Amount.Commodity == "" {
 		bm.EnvelopeRoot.Amount.Commodity = p.Amount.Commodity
 	}
+	if bm.ExpenseRoot.Amount.Commodity == "" {
+		bm.ExpenseRoot.Amount.Commodity = p.Amount.Commodity
+	}
 
 	// strip 'Expenses' from the path components
 	pathComponents := p.Account.PathComponents[1:]
-	account := bm.EnvelopeRoot.FindOrCreateAccount(pathComponents)
-	account.Postings = append(account.Postings, p)
 
-	// We can use whether the commodity is set to determine if we need to 'initialise' the account
-	if account.Amount.Commodity == "" {
-		// As this account is not the same as the non-budget expenses account version
-		// we need to ask it to create its path as it drops the 'Expenses:' head
-		account.Path = account.CreatePath()
-		account.PathComponents = pathComponents
+	// We want to mirror the accounts in the envelope and expense roots
+	// but when adding a posting for either one we don't want to add to it to the other
 
-		account.Amount.Commodity = p.Amount.Commodity
+	// Make the envelope account
+	envelopeAccount := bm.EnvelopeRoot.FindOrCreateAccount(pathComponents)
+	envelopeAccount.Amount.Commodity = p.Amount.Commodity
+
+	// Make the expense account
+	expenseAccount := bm.ExpenseRoot.FindOrCreateAccount(pathComponents)
+	expenseAccount.Amount.Commodity = p.Amount.Commodity
+
+	// Add to the expense account
+	expenseAccount.Postings = append(expenseAccount.Postings, p)
+
+	// As this account is not the same as the non-budget expenses account version
+	// we need to ask it to create its path as it drops the 'Expenses:' head
+	// TODO might not be necessary?
+	if len(envelopeAccount.Path) == 0 {
+		envelopeAccount.Path = envelopeAccount.CreatePath()
+		envelopeAccount.PathComponents = pathComponents
+	}
+	if len(expenseAccount.Path) == 0 {
+		expenseAccount.Path = expenseAccount.CreatePath()
+		expenseAccount.PathComponents = pathComponents
 	}
 
-	// Subtract the postings amount fro the account and all of its ancestors
-	if err := account.WalkAncestors(func(a *Account) error {
-		if err := a.Amount.Subtract(p.Amount); err != nil {
+	// Subtract the postings amount from the expense account and all of its ancestors
+	if err := expenseAccount.WalkAncestors(func(a *Account) error {
+		if err := a.Amount.Subtract(*p.Amount); err != nil {
 			return err
 		}
 		return nil
@@ -104,11 +167,11 @@ func (b *Budget) addIncomePosting(p *Posting) error {
 	bm.Income.Postings = append(bm.Income.Postings, p)
 
 	// We subtract to make the income positive
-	if err := bm.Income.Amount.Subtract(p.Amount); err != nil {
+	if err := bm.Income.Amount.Subtract(*p.Amount); err != nil {
 		return err
 	}
 
-	if err := bm.EnvelopeRoot.Amount.Subtract(p.Amount); err != nil {
+	if err := bm.EnvelopeRoot.Amount.Subtract(*p.Amount); err != nil {
 		return err
 	}
 
